50180e4f91a4bd160475ac757d32e2944df83172
Brahmaji2000 <112461127+Brahmaji2000@users.noreply.github.com>
2022-10-17 10:20:37 +0530

    Add files via upload

---
+++ Qes1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Cricket
+    {
+        public static void Main(string[] args)
+        {
+            Cricket e = new Cricket();
+        }
+        public void Pointscalculation()
+        {
+            int Total, Sum = 0, Value;
+            float Average;
+
+            Console.WriteLine("Enter number of matches");
+            Total = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < Total; i++)
+            {
+                Console.WriteLine("Enter the scores of matches: ");
+                Value = Convert.ToInt32(Console.ReadLine());
+                Sum += Value;
+            }
+
+            
+            Average = (float)Sum / Total;
+
+            Console.WriteLine("Sum: " + Sum + ", Average: " + Average);
+        }
+    }
+}
---
+++ Qes2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Box
+    {
+        int length;
+        int breadth;
+        public void Box1()
+        {
+            int L;
+            Console.WriteLine("Enter the length");
+            L = Convert.ToInt16(Console.ReadLine());
+        }
+        public void Box2()
+        {
+            int B;
+            Console.WriteLine("Enter the breadth");
+            B = Convert.ToInt16(Console.ReadLine());
+        }
+        public void add2box()
+        {
+            Box b = new Box();
+            b.Box1();
+            b.Box2();
+            int sum;
+        }
+
+        class Test
+        {
+            static void Main()
+            {
+                Box b = new Box();
+                b.Box1();
+                b.Box2();
+                b.add2box();
+            }
+        }
+    }
+}
